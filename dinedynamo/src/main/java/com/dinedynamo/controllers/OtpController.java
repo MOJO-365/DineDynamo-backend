4533cf33e9afcabf6244354b26f7e7a005733389
deepnsharma <sdeep9891@gmail.com>
2024-02-06 08:44:10 +0530

    8:44

--- dinedynamo/src/main/java/com/dinedynamo/controllers/OtpController.java
+++ dinedynamo/src/main/java/com/dinedynamo/controllers/OtpController.java
@@ -1,21 +1,25 @@
 package com.dinedynamo.controllers;
 
+import com.dinedynamo.api.ApiResponse;
 import com.dinedynamo.dto.OtpRequest;
 import com.dinedynamo.dto.OtpResponse;
 import com.dinedynamo.dto.OtpValidationRequest;
 import com.dinedynamo.services.SmsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class OtpController {
 
     @Autowired
     private SmsService smsService;
 
-    @GetMapping("/process")
+    @GetMapping("dinedynamo/process/otp")
     public String processSMS() {
         return "SMS sent";
     }
@@ -24,9 +28,20 @@ public class OtpController {
     public OtpResponse sendOtp(@RequestBody OtpRequest otpRequest) {
         return smsService.sendSMS(otpRequest);
     }
-    @PostMapping("/validate-otp")
-    public String validateOtp(@RequestBody OtpValidationRequest otpValidationRequest) {
-        return smsService.validateOtp(otpValidationRequest);
+    @PostMapping("dinedynamo/validate-otp")
+    public ResponseEntity<ApiResponse> validateOtp(@RequestBody OtpValidationRequest otpValidationRequest) {
+        String validateOtp = smsService.validateOtp(otpValidationRequest);
+
+        ApiResponse apiResponse;
+
+        if ("OTP is valid!".equals(validateOtp)) {
+            apiResponse = new ApiResponse(HttpStatus.OK, "OTP is valid", null);
+        } else {
+            apiResponse = new ApiResponse(HttpStatus.BAD_REQUEST, "OTP is invalid", null);
+        }
+
+        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
     }
+
 
 }
--- dinedynamo/src/main/java/com/dinedynamo/dto/OtpRequest.java
+++ dinedynamo/src/main/java/com/dinedynamo/dto/OtpRequest.java
@@ -3,11 +3,12 @@ package com.dinedynamo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class OtpRequest {
 
-    private String username;
     private String phoneNumber;
 }
