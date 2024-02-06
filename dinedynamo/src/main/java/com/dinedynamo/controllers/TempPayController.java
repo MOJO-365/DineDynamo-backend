65f9f736c266716821906846ed48a8ad790d1bd1
taishibra@gmail.com <taishibra@gmail.com>
2024-02-06 08:34:44 +0530

    Modified: Payement API

--- dinedynamo/src/main/java/com/dinedynamo/controllers/TempPayController.java
+++ dinedynamo/src/main/java/com/dinedynamo/controllers/TempPayController.java
@@ -1,43 +1,65 @@
 package com.dinedynamo.controllers;
 import com.dinedynamo.api.ApiResponse;
+import com.dinedynamo.dto.StripePayRequestDTO;
+import com.dinedynamo.helper.StripeCustomerUtil;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TempPayController {
 
     @PostMapping("dinedynamo/create-payment-intent")
-    public ResponseEntity<ApiResponse> createPaymentIntent() {
+    public String createPaymentIntent(@RequestBody StripePayRequestDTO stripePayRequestDTO) throws StripeException {
 
         Stripe.apiKey = "sk_test_51OZ7YrSHVgEh3MIN43YcgXCCwF4tHGLFwAlfrHG7cuC97tVDCWoEeggEBHQWUs8ywW1a91m69XH34DoMCu4ofXbu00XnlHSUX6";
 
+        Customer customer = StripeCustomerUtil.findOrCreateCustomer(stripePayRequestDTO.getCustomerEmail(), stripePayRequestDTO.getCustomerName());
+
+
         try {
-            PaymentIntentCreateParams params = new PaymentIntentCreateParams.Builder()
-                    .setCurrency("AUD")
-                    .setAmount(1000L)
-                    .setAutomaticPaymentMethods(
-                            PaymentIntentCreateParams.AutomaticPaymentMethods
-                                    .builder()
-                                    .setEnabled(true)
-                                    .build()
-                    )
-                    .build();
+            PaymentIntentCreateParams params =
+                    PaymentIntentCreateParams.builder()
+                            .setAmount(1000L)
+                            .setCurrency("AUD")
+                            .setCustomer(customer.getId())
+                            .setAutomaticPaymentMethods(
+                                    PaymentIntentCreateParams.AutomaticPaymentMethods
+                                            .builder()
+                                            .setEnabled(true)
+                                            .build()
+                            )
+                            .build();
 
             PaymentIntent paymentIntent = PaymentIntent.create(params);
 
-            return new ResponseEntity<>(new ApiResponse(HttpStatus.OK,"success",paymentIntent.getClientSecret()),HttpStatus.OK);
+            // Send the client secret from the payment intent to the client
+            return paymentIntent.getClientSecret();
+
+            //return new ResponseEntity<>(new ApiResponse(HttpStatus.OK,"success",paymentIntent.getClientSecret()),HttpStatus.OK);
 
         }
         catch (StripeException e) {
             System.out.println("StripeException OCCURRED");
-            return new ResponseEntity<>(new ApiResponse(HttpStatus.OK,"success",null),HttpStatus.OK);
+            return null;
+            //return new ResponseEntity<>(new ApiResponse(HttpStatus.OK,"success",null),HttpStatus.OK);
         }
     }
+
+
+//    static String calculateOrderAmount(Product[] items) {
+//        long total = 0L;
+//
+//        for (Product item: items) {
+//            // Look up the application database to find the prices for the products in the given list
+//            total += ProductDAO.getProduct(item.getId()).getDefaultPriceObject().getUnitAmountDecimal().floatValue();
+//        }
+//        return String.valueOf(total);
+//    }
+
 }
