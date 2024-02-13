38032b83d975fddc6f75076ae4e17d3c5f8f8b84
deepnsharma <sdeep9891@gmail.com>
2024-02-13 12:10:48 +0530

    12:10

--- dinedynamo/src/main/java/com/dinedynamo/controllers/InvoiceController.java
+++ dinedynamo/src/main/java/com/dinedynamo/controllers/InvoiceController.java
@@ -52,7 +52,7 @@ public class InvoiceController {
     @PostMapping("/dinedynamo/invoice/getorder")
     public ResponseEntity<ApiResponse> getFinalOrderForTable(@RequestBody Order order) {
         try {
-            List<Order> orderListForTable = orderRepository.findByTableId(order.getTableId());
+            List<Order> orderListForTable = orderRepository.findOrderList(order.getOrderList());
 
             if (!orderListForTable.isEmpty()) {
                 Order consolidatedOrder = consolidateOrders(orderListForTable);
@@ -60,6 +60,18 @@ public class InvoiceController {
             } else {
                 return new ResponseEntity<>(new ApiResponse(HttpStatus.NOT_FOUND, "No orders found for the specified table", null), HttpStatus.NOT_FOUND);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred", null), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PostMapping("dinedynamo/accept-order")
+    public ResponseEntity<ApiResponse> acceptOrder(@RequestBody Order order) {
+        try {
+            List<Order> orderListForTable = orderRepository.findByTableId(order.getTableId());
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.OK, "success", orderListForTable), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred", null), HttpStatus.INTERNAL_SERVER_ERROR);
--- dinedynamo/src/main/java/com/dinedynamo/repositories/OrderRepository.java
+++ dinedynamo/src/main/java/com/dinedynamo/repositories/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.dinedynamo.repositories;
 
 import com.dinedynamo.collections.Order;
+import org.json.simple.JSONArray;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -13,6 +14,8 @@ public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> findByRestaurantId(String restaurantId);
 
     List<Order> findByTableId(String tableId);
+
+    List<Order> findOrderList(JSONArray order);
 
 
 }
