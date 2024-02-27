fe07bec28689ed5047b02cad851cf94916dd705b
taishibra@gmail.com <taishibra@gmail.com>
2024-02-27 07:30:08 +0530

    Modified: trial pkg for testing date

--- dinedynamo/src/main/java/com/dinedynamo/trial/TrialCollection.java
+++ dinedynamo/src/main/java/com/dinedynamo/trial/TrialCollection.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
@@ -18,4 +19,7 @@ public class TrialCollection
     @Id
     String id;
     LocalDateTime localDateTime;
+    LocalTime localTime;
+
+
 }
