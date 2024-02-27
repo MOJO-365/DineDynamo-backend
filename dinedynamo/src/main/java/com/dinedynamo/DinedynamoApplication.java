e1df01c42dfb3a096d1af2997179711dd2531ea6
taishibra@gmail.com <taishibra@gmail.com>
2024-02-27 09:51:35 +0530

    .

--- dinedynamo/src/main/java/com/dinedynamo/DinedynamoApplication.java
+++ dinedynamo/src/main/java/com/dinedynamo/DinedynamoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
 @SpringBootApplication
 
 @EnableConfigurationProperties
@@ -26,7 +25,6 @@ public class DinedynamoApplication
 	public static void main(String[] args) {
 		SpringApplication.run(DinedynamoApplication.class, args);
 		System.out.println("Hello, this application is up and running");
-
 	}
 
 
