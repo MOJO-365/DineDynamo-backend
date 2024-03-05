7dc2852fdc7cc22f3e732e077a09dd8d796081dc
Hetvik Shah <hetvikshah11@gmail.com>
2024-03-05 10:23:36 +0530

    10.23

--- dinedynamo/src/main/java/com/dinedynamo/controllers/invoice_controllers/TakeAwayBillController.java
+++ dinedynamo/src/main/java/com/dinedynamo/controllers/invoice_controllers/TakeAwayBillController.java
@@ -7,6 +7,7 @@ import com.dinedynamo.services.invoice_services.TakeAwayBillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 @RestController
+@CrossOrigin("*")
 public class TakeAwayBillController {
 
     @Autowired
